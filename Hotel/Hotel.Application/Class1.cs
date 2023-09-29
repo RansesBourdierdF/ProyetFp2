8d6edf2a6394552d4172fd6a971c570c8e429b49
Ranses Bourdierd <ransesbf@icloud.com>
2023-09-28 23:24:34 -0400

    P2DI

--- Hotel/Hotel.Application/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Hotel.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Hotel/Hotel.Domain/Core/Person.cs
@@ -0,0 +1,13 @@
+﻿using School.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.Domain.Core
+{
+    public abstract class Person : BaseEntity
+    {
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+    }
+}
