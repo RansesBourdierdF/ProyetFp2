26e31d22ec947d07c393c7e8e34e3f6eb761e4d5
madelinej3 <144567185+madelinej3@users.noreply.github.com>
2023-09-30 12:00:51 -0400

    TODO en infraestructura

--- Hotel/Hotel.Domain/Core/BaseEntity.cs
+++ Hotel/Hotel.Domain/Core/BaseEntity.cs
@@ -7,11 +7,11 @@ namespace Hotel.Domain.Core
     public abstract class BaseEntity
     {
         public DateTime CreationDate { get; set; }
-        public int CreationUser { get; set; }
-        public DateTime? ModifyDate { get; set; }
-        public int? UserMod { get; set; }
-        public int? UserDeleted { get; set; }
-        public DateTime? DeletedDate { get; set; }
+        public int Creacionusuario { get; set; }
+        public DateTime? ModificacionFecha { get; set; }
+        public int? UsuarioMod { get; set; }
+        public int? EliminacionDeUsuario { get; set; }
+        public DateTime? EliminacionDeFecha { get; set; }
         public bool Deleted { get; set; }
     }
 }
