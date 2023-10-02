bfd547fdb1e282c8b78d37ed18cae7e7019e43ef
summerssheep <144567185+madelinejaelp3@users.noreply.github.com>
2023-10-02 02:12:00 -0400

    Hotel Context

--- Hotel/Hotel.Infrastructrure/Context/HotelContext.cs
+++ Hotel/Hotel.Infrastructrure/Context/HotelContext.cs
@@ -1,4 +1,26 @@
-﻿
+﻿using Hotel.Infrastructure.Context;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Hotel.Infrastructure.Context
+{
+    public class HotelContext : DbContext
+    {
+        public HotelContext(DbContextOptions<HotelContext> options) : base(options)
+        {
+        
+
+    }
+
+    public DbSet<Cliente> Clientes { get; set; } = null!;
+        public DbSet<Categoria> Categorias { get; set; } = null!;
+
+        public class Cliente
+        {
+            public int Id { get; set; }
+            // Otras propiedades del cliente
+        }
+
         public class Categoria
         {
             public int Id { get; set; }
@@ -6,16 +28,28 @@
         }
     }
 
-    public class DbContext
+    public class DbSet<T>
     {
-        public DbContext(DbContextOptions<HotelContext> options)
-        {
-            Options = options;
-        }
+    }
 
-        public DbContextOptions<HotelContext> Options { get; }
+    // DbSet personalizado no parece ser necesario en este contexto.
+    // public class DbSet<T>
+    // {
+    // }
+}
+
+[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+public class DbContext
+{
+    public DbContext(DbContextOptions<HotelContext> options)
+    {
+        Options = options;
+    }
+
+    public DbContextOptions<HotelContext> Options { get; }
 
     private string GetDebuggerDisplay()
     {
+        return ToString();
     }
 }
