0d9c94cfaa0001494a6db7f621e956f89ad89f23
summerssheep <144567185+madelinejaelp3@users.noreply.github.com>
2023-10-24 22:54:57 -0400

    API

--- Hotel/Hotel.Api/Controllers/ClienteController.cs
+++ Hotel/Hotel.Api/Controllers/ClienteController.cs
@@ -1,6 +1,62 @@
 ﻿namespace Hotel.Api.Controllers
 {
-    public class ClienteController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase  // Cambia el nombre del controlador a ClienteController
+    {
+        private readonly ClienteRepository clienteRepository;  // Cambia el nombre de la variable y el tipo de repositorio
+
+        public ClienteController(ClienteRepository clienteRepository)  // Cambia el nombre del parámetro del constructor
+        {
+            this.clienteRepository = clienteRepository;
+        }
+
+        [HttpGet("GetClienteByClienteId")]
+        public IActionResult GetClienteByClienteId(int clienteId)  // Cambia los nombres de las rutas y parámetros
+        {
+            var cliente = clienteRepository.GetClienteById(clienteId);
+
+            if (cliente != null)
+            {
+                return Ok(cliente);
+            }
+            else
+            {
+                // Puedes devolver un código de estado 404 (Not Found) si el Cliente no se encuentra.
+                return NotFound();
+            }
+        }
+
+        [HttpGet("GetCliente")]
+        public IActionResult GetCliente()
+        {
+            var clientes = clienteRepository.GetEntities().Select(dc => new ClienteGetAllModel()
+            {
+                Descripcion = dc.Descripcion,
+                // Otros campos relacionados con la entidad Cliente
+            }).ToList();
+            return Ok(clientes);
+        }
+
+        [HttpPost("SaveCliente")]
+        public IActionResult Post([FromBody] ClienteAddModel clienteAdd)
+        {
+            Cliente cliente = new Cliente()
+            {
+                CreacionDeUsuario = clienteAdd.CreacionDeUsuario, // Asegúrate de asignar un valor válido
+                NombreCompleto = clienteAdd.NombreCompleto,
+                FechaCreacion = clienteAdd.FechaCreacion,
+                Estado = clienteAdd.Estado
+            };
+
+            this.clienteRepository.Save(cliente);
+            return Ok();
+        }
+
+        [HttpPost("UpdateCliente")]
+        public IActionResult Post([FromBody] ClienteUpdateModel clienteUpdate)
+        {
+            Cliente cliente = new Cliente()
     {
     }
 }
