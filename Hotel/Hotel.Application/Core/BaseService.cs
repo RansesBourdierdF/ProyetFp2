3650b8bca5f0ae2981ca7215cfdb34f6d42b015e
summerssheep <144567185+madelinejaelp3@users.noreply.github.com>
2023-11-03 22:57:19 -0400

    Application

---
+++ Hotel/Hotel.Application/Core/BaseService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.Application.Core
+{
+    internal class BaseService
+    {
+    }
+}
---
+++ Hotel/Hotel.Application/Dtos/DtoBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.Application.Dtos
+{
+    internal class DtoBase
+    {
+    }
+}
