dcf1aa180c99b1483a718ea06894ca1fef2b1dbb
Jandyguerrero9 <Jandyguerrero9@gmail.com>
2023-11-09 08:15:47 -0400

    api3

--- Hotel/Hotel.Api/Program.cs
+++ Hotel/Hotel.Api/Program.cs
@@ -1,6 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using Hotel.Domain.Repository;
+using Hotel.Infrastructrure.Context;
+using Hotel.Infrastructrure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+
+//agregar dependencia del contexto//
+
+builder.Services.AddDbContext<HotelContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("HotelContext")));
+
+//Dependencia de los Repositorios//
+
+builder.Services.AddTransient<IEstadoHabitacionRepository, EstadoHabitacionRepository>();
+builder.Services.AddTransient<IHabitacionRepository, HabitacionRepository>();
+
+//Dependencia de los App Services//
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- Hotel/Hotel.Infrastructrure/Context/HotelContext.cs
+++ Hotel/Hotel.Infrastructrure/Context/HotelContext.cs
@@ -6,7 +6,9 @@ namespace Hotel.Infrastructrure.Context
 {
     public class HotelContext : DbContext
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public HotelContext(DbContextOptions<HotelContext> options) : base(options)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
 
         }
